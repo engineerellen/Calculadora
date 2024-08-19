807dbbbbd836fce753a3425bdc1e917e3e74a74c
Ellen <36012338+engineerellen@users.noreply.github.com>
2024-08-19 19:56:13 -0300

    Add project files.

---
+++ Domain/Calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Domain
+{
+    public class Calculadora
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+
+        public Calculadora(double _numero1, double _numero2)
+        {
+            Numero1 = _numero1;
+            Numero2 = _numero2;
+        }
+
+        public double Somar() => Numero1 + Numero2;
+
+        public double Subtrair() => Numero1 - Numero2;
+
+        public double Multiplicar() => Numero1 * Numero2;
+
+        public double Dividir()
+        {
+            if (Numero2 == 0)
+                throw new DivideByZeroException();
+
+            return Numero1 / Numero2;
+        }
+    }
+}
---
+++ MostrarCalculadora/Program.cs
@@ -0,0 +1,7 @@
+﻿using Domain;
+
+Calculadora calc = new Calculadora(5, 3);
+
+Console.WriteLine($"Soma: {calc.Somar()}");
+
+Console.WriteLine($"Subtracao: {calc.Subtrair()}");
